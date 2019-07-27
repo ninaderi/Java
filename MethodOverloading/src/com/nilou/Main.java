b73a7456e43d20dee000365205dd542b64217fbd
ninaderi <nina8867@gmail.com>
2019-07-27 17:25:51 -0600

    At folk festival!

---
+++ MethodOverloading/src/com/nilou/Main.java
@@ -0,0 +1,27 @@
+package com.nilou;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int newScore = calculateScore("Folk", 397);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore();
+
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name, no player score.");
+        return 0; //We can't return anything because we have no parameters
+    }
+}
--- SpeedConverter/src/SpeedConverter.java
+++ SpeedConverter/src/SpeedConverter.java
@@ -9,6 +9,11 @@ public class SpeedConverter {
     public static void printConversion (double kilometersPerHour) {
         if(kilometersPerHour < 0) {
             System.out.println("Invalid Value");
+        } else {
+            long milesPerhour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour +
+                    " km/h = " + milesPerhour +
+                    " mi/h");
         }
     }
 }
