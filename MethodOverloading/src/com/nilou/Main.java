9971913d77d7a593b8eb679631327d2b39823d1c
ninaderi <nina8867@gmail.com>
2019-08-01 15:13:37 -0600

    Aug 01

--- MethodOverloading/src/com/nilou/Main.java
+++ MethodOverloading/src/com/nilou/Main.java
@@ -7,6 +7,31 @@ public class Main {
         System.out.println("New score is " + newScore);
         calculateScore(75);
         calculateScore();
+        calcFeetAndInchesToCentimeters(6, 0);
+        calcFeetAndInchesToCentimeters(100);
+
+    }
+    public static double calcFeetAndInchesToCentimeters (double feet, double inches) {
+
+        if((feet < 0) ||((inches < 0)) || ((inches > 12))) {
+            System.out.println("Invalid feet or inches entered");
+            return -1;
+        }
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + "feet," + inches + "inches = " + centimeters + " cm");
+        return centimeters;
+    }
+
+    public static double calcFeetAndInchesToCentimeters (double inches) {
+        if(inches < 0) {
+            return -1;
+        }
+
+        double feet = (int) inches /12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inches is equal to " + feet + " feet and " + remainingInches);
+        return calcFeetAndInchesToCentimeters(feet, remainingInches);
 
     }
 
